700437031b0d633e4b5add3559e64b39d7679e64
Hongji <hongji667719@gmail.com>
2023-10-22 14:47:29 -0700

    added resume entries

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -16,7 +16,7 @@ const work = [
     name: 'Hack Health',
     position: 'software engineer',
     url: '',
-    startDate: '2023-05',
+    startDate: '2023-04',
     summary: 'PosePro is the application that I developed at this health communication startup.',
     highlights: [
       'developed PosePro,an app that detects posture‐related conditions and provides real‐time prompts and tips for improving,usingDjango,React.',
@@ -29,11 +29,28 @@ const work = [
     name: 'KODEE',
     position: 'Co-founder',
     url: '',
-    startDate: '2023-07',
+    startDate: '2023-06',
     summary: 'Kodee is a language service application that aims to become best ally to conference interpreters.',
     highlights: [
       'developed an interpretation tool using Spring, React and MUI that displays real‐time terminology prompts through glossaries.',
       'leveraged Web Speech API for speech transcription, deployed LibreTranslate on a local server with RESTful APIs to improve accuracy by 23%.',
+      'managed user login with JWT tokens and contextAPI to encrypt user information,resulting in a 13% increase in user numbers.',
+      'managed glossaries with PostgreSQL for CRUD operations and Redis for recently accessed entries to increase response time by 30%.',
+      'containerized the frontend and backend using Docker,then deployed with Jenkins and Kubernetes.',
+    ],
+  },
+  {
+    name: 'SCU Imaginarium Lab',
+    position: 'Research Assistant',
+    url: '',
+    startDate: '2023-03',
+    summary: 'Kodee is a language service application that aims to become best ally to conference interpreters.',
+    highlights: [
+      'executed distributed crawls using Scrapy‐Redis to generate a proprietary dataset for training, resulting in a 120% increase in dataset size.',
+      `developed a Flask backend application that turns a movement clip into an avatar animation with OpenPose,pandas,Blender python scripts
+and Git for version control, leading to a stronger lab portfolio and an increase in funding by 12%.`,
+      'utilized Conda for environment control and deployed comparable 3D pose estimation models on a Unix server with Pytorch for comparison.',
+      'led a team of six to leverage NumPy, scipy, and OpenCV to detect and clean NaN, augment the datasets with random masks,normalize the reference system and merge different datasets, which led to a paper accepted by IROS, a Robotics and AI conference with 78 h5‐index.',
     ],
   },
 ];
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,11 +11,7 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2><Link to="/">About this site</Link></h2>
-          <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
-          </p>
+          <h2><Link to="/">About</Link></h2>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more about me,
@@ -24,7 +20,31 @@ const Index = () => (
         <Link to="/projects">projects</Link> {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p>
+        My career path has been fulfilling and exciting. At Hack Health, I contributed
+        to various modules, most notably developing PosePro—an app that offers real-time posture
+        improvement prompts using advanced technologies like Django and React.
+        I am proud to have led initiatives that resulted in significant upticks
+        in user retention and drastically improved accuracy and response times.
+      </p>
+      <p>
+        I am actively involved in research at the SCU Imaginarium Lab. Here,
+        I have executed distributed crawls to bolster our dataset and developed
+        applications that have significantly contributed to the success of the lab,
+        leading to a paper acceptance by the esteemed IROS conference.
+      </p>
+      <p>
+        On the project front, KODEE—a language service application—is
+        my most recent endeavor. With features ranging from real-time terminology
+        prompts to enhanced speech transcription, this project perfectly encapsulates
+        my dedication to crafting efficient and user-friendly solutions.
+      </p>
+      <p>
+        My toolbox includes a rich array of languages, frameworks, and other
+        technical skills which I have harnessed throughout my academic and professional
+        journey. From harnessing Scrapy crawlers to containerizing applications
+        using Docker, my technical journey has been both diverse and fulfilling.
+      </p>
     </article>
   </Main>
 );
